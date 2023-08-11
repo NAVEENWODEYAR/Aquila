4e7538af29571014363b465633e36fa5e19f973e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-11 15:05:58 +0530

    first

---
+++ com.bonelli/src/main/java/com/bonelli/BonelliApplication.java
@@ -0,0 +1,13 @@
+package com.bonelli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonelliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonelliApplication.class, args);
+	}
+
+}
---
+++ com.bonelli/src/test/java/com/bonelli/BonelliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonelli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonelliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
