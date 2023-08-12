d9e9ab4b3e0a0dd25ed78a7e9a66c8d19f1005b9
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-13 04:08:47 +0530

    custom queries

--- com.bonelli/src/main/java/com/bonelli/repo/CarRepo.java
+++ com.bonelli/src/main/java/com/bonelli/repo/CarRepo.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
+
 //public interface CarRepo extends MongoRepository<Car,String>
 public interface CarRepo extends JpaRepository<Car,Integer>
 {
-    /** @Query, custom queries.,
-    @Query(value = "SELECT * FROM cars WHERE cars.carName=carName")
+    /** @Query, custom queries., */
+    @Query(value = "SELECT * FROM cars WHERE cars.carName=carName",nativeQuery = true)
     Car findByName(String carName);
-    */
+
+    @Query(value = "SELECT * FROM cars ORDER BY cars.carName DESC",nativeQuery=true)
+    List<Car> findByDescOrder();
 }
