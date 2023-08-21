715075a24f234806333f83b02ac1c9095d2d9e8b
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-21 12:37:14 +0530

    tawny

---
+++ Tawny/src/test/java/com/tawny/TawnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TawnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
