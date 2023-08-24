c009c4b97f1a71712776464d63b2b06574f18ca1
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-24 16:22:50 +0530

    runtime

--- UI/bonelli/src/Nav/UpdateBook.jsx
+++ UI/bonelli/src/Nav/UpdateBook.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const UpdateBook = () => {
 
   // to store the data in 
-  const[book,setBooks] = useState([]);
+  const[cars,setCars] = useState([]);
 
   // render the page each time,
   useEffect(()=> {},[]);
@@ -12,13 +12,48 @@ const UpdateBook = () => {
   let booksData = async () => {
         let result = await axios.get("http://localhost:96/getCars");
         console.log(result.data);
-        setBooks(result.data);
+        setCars(result.data);
   }
 
 
   return(
     <>
-
+        <div className="container" style={{marginTop:"20px"}}>
+          <table className='table table-hover table-bordered'>
+            <caption> LuxuRy Cars </caption>
+            <thead className='thead-dark'>
+              <tr>
+                <th> Serial NO </th>
+                <th> Car Id </th>
+                <th> Car Company </th>
+                <th> Car Name </th>
+                <th> Car Type </th>
+                <th> Car Model </th>
+                <th> Car Price </th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                cars.length >0 ?
+                <>
+                  {
+                    cars.map((car)=> {
+                      return <tr>
+                                  <td>{car.carId}</td>
+                                  <td>{car.carMake}</td>
+                                  <td>{car.carName}</td>
+                                  <td>{car.carType}</td>
+                                  <td>{car.carModel}</td>
+                                  <td>{car.carPrice}</td>
+                      </tr>
+                    })
+                  }
+                </>
+                :<h2> No data in the database.., </h2>
+              }
+            </tbody>
+          </table>
+        </div>
     </>
   )
 }
