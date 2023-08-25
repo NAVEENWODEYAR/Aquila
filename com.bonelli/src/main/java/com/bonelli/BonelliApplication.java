2bc5547a681e87f6ba78fdefac6bd7e8939b0f10
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-25 14:43:52 +0530

    Bonellis Eagle

--- com.bonelli/src/main/java/com/bonelli/BonelliApplication.java
+++ com.bonelli/src/main/java/com/bonelli/BonelliApplication.java
@@ -10,6 +10,7 @@ public class BonelliApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(BonelliApplication.class, args);
+		System.out.println(" about 60 cm (24 inches) long, is dark above and light below, has a broad tailband, and usually shows a white patch on the back.");
 	}
 
 }
